0472e9387cd3a02b2f29a632c60919614fc5371b
Raghu Betina <raghu@firstdraft.com>
2019-06-13 05:15:33 +0000

    Whitelist columns in Meal for ActiveAdmin

--- app/admin/meals.rb
+++ app/admin/meals.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Meal do
+
+ permit_params :name, :restaurant_id, :image_url
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
